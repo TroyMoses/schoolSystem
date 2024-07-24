a8562258c81d4fbeff0bec250f2fb2393c942b75
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-07-24 11:12:17 -0700

    fontend

--- frontend/src/redux/teacherRelated/teacherHandle.js
+++ frontend/src/redux/teacherRelated/teacherHandle.js
@@ -8,7 +8,7 @@ import {
     doneSuccess
 } from './teacherSlice';
 
-const REACT_APP_BASE_URL = "http://localhost:5000";
+const REACT_APP_BASE_URL = "https://schoolsystem-ice2.onrender.com";
 
 export const getAllTeachers = (id) => async (dispatch) => {
     dispatch(getRequest());
