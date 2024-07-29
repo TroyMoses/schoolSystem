39f566a03c1978fbdfe1b83f9cbdccfeed161b00
TroyMoses <mugabimoses07@gmail.com>
2024-07-29 17:07:39 +0800

    year

---
+++ backend/models/yearSchema.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const yearSchema = new mongoose.Schema({
+    yearName: {
+        type: String,
+        required: true,
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model("sclass", yearSchema);
+
