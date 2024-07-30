437c4c1308fea3afee7866109a3d6caffe86ad77
TroyMoses <mugabimoses07@gmail.com>
2024-07-30 18:39:30 +0800

    Update classTeacherCommentSchema.js

--- backend/models/classTeacherCommentSchema.js
+++ backend/models/classTeacherCommentSchema.js
@@ -1,10 +1,6 @@
 const mongoose = require("mongoose");
 
 const classTeacherSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
     from: {
         type: Number,
         required: true,
