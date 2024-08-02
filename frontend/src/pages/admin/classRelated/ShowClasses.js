d4841239418bb9f2b0fbbca533bd1a6eab223972
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-08-02 08:09:20 -0700

    marks

--- frontend/src/pages/admin/classRelated/ShowClasses.js
+++ frontend/src/pages/admin/classRelated/ShowClasses.js
@@ -78,7 +78,7 @@ const ShowClasses = ({situation}) => {
       { icon: <PersonAddAlt1Icon />, name: 'Add Student', action: () => navigate("/Admin/class/addstudents/" + row.id) },
       { icon: <GradeIcon />, name: 'Add BOT', action: () => navigate("/Admin/addsubject/" + row.id) },
       { icon: <GradeIcon />, name: 'Add MID', action: () => navigate("/Admin/class/addstudents/" + row.id) },
-      { icon: <GradeIcon />, name: 'Add END', action: () => navigate("/Admin/class/addstudents/" + row.id) },
+      // { icon: <GradeIcon />, name: 'Add END', action: () => navigate("/Admin/class/addstudents/" + row.id) },
       { icon: <GradeIcon />, name: 'Add END', action: () => navigate("/Admin/students/chooseentrysubject/" + row.id)}, 
     ];
     // const actions = [
@@ -95,10 +95,10 @@ const ShowClasses = ({situation}) => {
           onClick={() => navigate("/Admin/classes/class/" + row.id)}>
           View
         </BlueButton>
-        <PurpleButton variant="contained"
+        {/* <PurpleButton variant="contained"
             onClick={() => navigateHandler(row.id)}>
             Choose
-        </PurpleButton>
+        </PurpleButton> */}
         <ActionMenu actions={actions} />
       </ButtonContainer>
     );
