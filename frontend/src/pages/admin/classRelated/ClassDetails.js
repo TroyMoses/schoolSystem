2cce6f6998b45794987f603c1339e43d946aac92
Eng Lubanjwa Francis <lubanjwafrancis@gmail.com>
2024-08-23 11:03:03 -0700

    conduct dane

--- frontend/src/pages/admin/classRelated/ClassDetails.js
+++ frontend/src/pages/admin/classRelated/ClassDetails.js
@@ -663,7 +663,7 @@ const ClassDetails = () => {
         <select
             name={row.id}
             className="marksInput border border-gray-300 rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-yellow-500"
-            value={disciplineByStudent[row.id] || ""}
+            value={attendanceByStudent[row.id] || ""}
             onChange={handleChange}
             defaultValue=""
             >
