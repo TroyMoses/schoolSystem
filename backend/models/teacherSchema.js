0d6ba053b7d651fbd2510264c57f32de62fcfb3c
Troy Moses Mugabi <mugabimoses07@gmail.com>
2024-08-27 22:45:56 +0800

    Update teacherSchema.js

--- backend/models/teacherSchema.js
+++ backend/models/teacherSchema.js
@@ -32,18 +32,18 @@ const teacherSchema = new mongoose.Schema({
         ref: 'sclass',
         required: true,
     },
-    attendance: [{
-        date: {
-            type: Date,
-            required: true
-        },
-        presentCount: {
-            type: String,
-        },
-        absentCount: {
-            type: String,
-        }
-    }]
+    // attendance: [{
+    //     date: {
+    //         type: Date,
+    //         required: true
+    //     },
+    //     presentCount: {
+    //         type: String,
+    //     },
+    //     absentCount: {
+    //         type: String,
+    //     }
+    // }]
 }, { timestamps: true });
 
 module.exports = mongoose.model("teacher", teacherSchema)
